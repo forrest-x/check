e1d6ce902a485d7bb8b103b74c8f339f7c052fbe
Forrest ࿆²⁰²⁰ <huangleizzz@qq.com>
2022-06-28 16:04:42 +0800

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -5,6 +5,9 @@ const checkIn = async (cookie) => {
         method: 'post',
         url: 'https://glados.rocks/api/user/checkin',
         headers: {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
             'Cookie': cookie
         },
         data: {
