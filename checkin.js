d4cfa18da17436e78c207e91329584cacc6e827b
Forrest ࿆²⁰²⁰ <huangleizzz@qq.com>
2022-06-28 16:06:04 +0800

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -7,7 +7,7 @@ const checkIn = async (cookie) => {
         headers: {
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'accept-encoding': 'gzip, deflate, br',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
             'Cookie': cookie
         },
         data: {
